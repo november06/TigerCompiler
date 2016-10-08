e3926a30c78b2aa4671f255286e823f5c6e96175
Xuan LU (Shane) <shanelu1106@gmail.com>
2016-10-09 00:49:33 +1300

    Fixing cup errors.

--- cup.bat
+++ cup.bat
@@ -2,7 +2,7 @@ call %~dp0SetupJava.bat
 set CUP_HOME=C:\trash\workspace\cup
 set CLPATH=%JAVA_HOME%\lib\classes.zip;%CUP_HOME%
 @echo on
-%JAVA_HOME%\bin\java -classpath %CLPATH% java_cup.Main .\cup\tiger.cup 
+%JAVA_HOME%\bin\java -classpath %CLPATH% java_cup.Main -expect 2 .\cup\tiger.cup 
 move .\parser.java .\tiger\parse
 move .\sym.java .\tiger\parse
 xcopy ..\cup\java_cup\runtime\*.java .\java_cup\runtime\ /Y
