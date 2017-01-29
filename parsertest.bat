27717df239ff165c7af476dbf5b61da0db0b5401
Xuan LU (Shane) <shanelu1106@gmail.com>
2017-01-30 00:18:05 +1300

    Add testcases

--- parsertest.bat
+++ parsertest.bat
@@ -1,10 +1,1 @@
-pushd bin
-
-REM C:\Users\Shane\Downloads\Tiger\tiger_runnable\cuptest.txt will throw exception
-xcopy C:\Users\Shane\Downloads\Tiger\Testcases\Official\Good\test1.tig .\cuptest.txt /Y
-
-java tiger.parse.CupTest
-
-pause
-
-popd
+call python parsertest.py %~dp0.
---
+++ parsertest.py
@@ -0,0 +1,37 @@
+# dir walker for testcases
+import os
+import sys
+
+if len(sys.argv) < 2: 
+    print "expect the current directory" 
+
+testCaseRoot = os.path.join(sys.argv[1], ".\\testcases")
+workingDir = os.path.join(sys.argv[1], ".\\bin")
+os.chdir(workingDir)
+tempfile = os.path.join(sys.argv[1], ".\\testoutput")
+resultfile = os.path.join(sys.argv[1], ".\\testresult.txt")
+if os.path.isfile(resultfile):
+    os.remove(resultfile)
+
+totalCases = 0
+exceptionalCases = 0
+
+# traverse root directory, and list directories as dirs and files as files
+for root, dirs, files in os.walk(testCaseRoot):
+    for f in files:
+        totalCases = totalCases + 1
+        currentPath = os.path.join(root, f)
+        print currentPath
+        ret = os.system("java tiger.parse.CupTest " + currentPath + " >" + tempfile + " 2>&1" )
+        if ret != 0: 
+            os.system("echo " + currentPath +" >>" + resultfile)
+            os.system("type " + currentPath  + " >>" + resultfile)
+            os.system("type " + tempfile + " >>" + resultfile)
+            os.system("echo --------------------------------------- >>" + resultfile)
+            exceptionalCases = exceptionalCases + 1
+            
+os.system("echo " + str(exceptionalCases) + " / " + str(totalCases) + " >> " + resultfile)
+
+
+os.remove(tempfile)
+os.chdir(sys.argv[1])
